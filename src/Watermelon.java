2f3bbd185c414f11dca17e08a48dbd98318fc2fe
jushack <3.jushack.1@gmail.com>
2022-03-21 23:13:19 +0900

    4A

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class n4A {
+public class Watermelon {
 
 	public static void main(String[] args) throws IOException {
 		
